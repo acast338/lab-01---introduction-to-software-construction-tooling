47d67352acfaa39bc87e5a9379d75fbd626b8c70
acast338 <acast338@ucr.edu>
2022-04-05 16:20:24 -0700

    Add third test

--- test.cpp
+++ test.cpp
@@ -12,6 +12,12 @@ TEST(EchoTest, EmptyString) {
     EXPECT_EQ("", echo(1,test_val));
 }
 
+TEST(EchoTest, MyNameIsAlyssa) {
+    char* test_val[6]; test_val[0] = "./c-echo"; test_val[1] = "My"; test_val[2] = "name"; test_val[3] = "is"; test_val[4] = "Alyssa"; test_val[5] = ".";
+    EXPECT_EQ("My name is Alyssa .", echo(6,test_val));
+}
+
+
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
