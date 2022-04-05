0eeba08780679b658b22fb6c314af39fba3c2722
acast338 <acast338@ucr.edu>
2022-04-05 14:14:33 -0700

    Make a commit here with CMakeLists.txt, c-echo.h, main2.cpp, and the updated .gitignore.

--- main2.cpp
+++ main2.cpp
@@ -1,4 +1,4 @@
-nclude "c-echo.h"
+#include "c-echo.h"
 
 int main(int argv, char** argc) {
     std::cout << echo(argv, argc);
--- src/main1.cpp
+++ src/main1.cpp
@@ -5,9 +5,9 @@ using namespace std;
 
 int main()
 {
-    Rectangle rect;
-    rect.set_width(3);
-    rect.set_height(4);
+    Rectangle rect1, rect2;
+    rect1.set_width(3);
+    rect1.set_height(4);
 
     rect2.set_width(4);
     rect2.set_height(2);
